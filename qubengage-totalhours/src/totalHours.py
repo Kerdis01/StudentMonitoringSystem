b304fee0b9e2812afa254683a3ec2f68008f8595
Kyle Erdis <kerdis01@qub.ac.uk>
2023-12-05 17:59:38 +0000

    TotalHours function fixed!

--- qubengage-totalhours/src/totalHours.py
+++ qubengage-totalhours/src/totalHours.py
@@ -1,20 +1,21 @@
 from flask import Flask, jsonify, request
-from flask_cors import CORS  # Import the CORS module
+from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
+CORS(app)
 
 def calculate_total_hours(attendances):
-    
+    attendances = [int(value) for value in attendances]
     total_hours = sum(attendances)
     return total_hours
 
 @app.route('/calculate_total_hours', methods=['POST'])
 def calculate_total_hours_route():
-    data = request.json  # Assuming the data is sent as JSON
+    data = request.json
     attendances = data.get("attendances", [])
     total_hours = calculate_total_hours(attendances)
-    return jsonify({"total_hours": total_hours}), total_hours
+    
+    return jsonify({"total_hours": total_hours}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
