658772e078b4978b9cc2ee2c466934c9b46a81d2
Kyle Erdis <kerdis01@qub.ac.uk>
2023-12-13 02:24:11 +0000

    fixing test case

--- qubengage-maxmin/tests/maxMinFunctionsTest.php
+++ qubengage-maxmin/tests/maxMinFunctionsTest.php
@@ -7,8 +7,7 @@ use function QubengageMaxmin\getMaxMin;
 
 class maxMinFunctionsTest extends TestCase
 {
-    public function testWithValidInputAndDifferentAttendances()
-    {
+    public function testWithValidInputAndDifferentAttendances() {
         $items = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
         $attendances = [10, 20, 30, 40];
         $max_min_items = getMaxMin($items, $attendances);
@@ -16,8 +15,7 @@ class maxMinFunctionsTest extends TestCase
         $this->assertEquals('Item 1 - 10', $max_min_items[1]);
     }
 
-    public function testWithEmptyArraysExpectingEmptyStrings()
-    {
+    public function testWithEmptyArraysExpectingEmptyStrings() {
         $items = [];
         $attendances = [];
         $max_min_items = getMaxMin($items, $attendances);
@@ -25,25 +23,22 @@ class maxMinFunctionsTest extends TestCase
         $this->assertEquals('', $max_min_items[1]);
     }
 
-    public function testWithEqualAttendancesExpectingFirstAndLast()
-    {
+    public function testWithEqualAttendancesExpectingFirstAndLast() {
         $items = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
         $attendances = [20, 20, 20, 20];
         $max_min_items = getMaxMin($items, $attendances);
-        $this->assertEquals('Item 1 - 20', $max_min_items[1]);
-        $this->assertEquals('Item 4 - 20', $max_min_items[0]);
+        $this->assertEquals('Item 1 - 20', $max_min_items[0]);
+        $this->assertEquals('Item 4 - 20', $max_min_items[1]);
     }
 
-    public function testWithUnequalArrayLengthsExpectingException()
-    {
+    public function testWithUnequalArrayLengthsExpectingException() {
         $this->expectException(\InvalidArgumentException::class);
         $items = ['Item 1', 'Item 2', 'Item 3'];
         $attendances = [20, 20];
         getMaxMin($items, $attendances);
     }
 
-    public function testWithNegativeAttendances()
-    {
+    public function testWithNegativeAttendances() {
         $items = ['Item 1', 'Item 2', 'Item 3'];
         $attendances = [-10, -20, -30];
         $max_min_items = getMaxMin($items, $attendances);
@@ -51,8 +46,7 @@ class maxMinFunctionsTest extends TestCase
         $this->assertEquals('Item 3 - -30', $max_min_items[1]);
     }
 
-    public function testWithSingleItem()
-    {
+    public function testWithSingleItem() {
         $items = ['Item 1'];
         $attendances = [10];
         $max_min_items = getMaxMin($items, $attendances);
