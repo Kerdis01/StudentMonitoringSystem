85dd181b390e88ebc8c6624a42ae1aef4d96af1e
Kyle Erdis <kerdis01@qub.ac.uk>
2023-12-13 19:46:27 +0000

    Dockerfile updates for engagementscore and updates to totalHours code in frontend and backend service

--- qubengage-totalhours/src/totalHours.py
+++ qubengage-totalhours/src/totalHours.py
@@ -22,4 +22,4 @@ def calculate_total_hours_route():
     return jsonify({"total_hours": total_hours}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
